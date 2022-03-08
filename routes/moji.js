7be84b6ea13e38774d2f4c54a062ac318239a62a
Iori Takeshita <takeshitaiori@gmail.com>
2022-03-08 10:06:51 -0800

    link

--- routes/moji.js
+++ routes/moji.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
 const { getRecipes } = require('../controller/moji')
 
 router.get('https://emoji-meal-backend.herokuapp.com/findRecipe', getRecipes)
