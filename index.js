497a77152799130fe20123b1492065dd1c94099c
Iori Takeshita <takeshitaiori@gmail.com>
2022-03-13 20:08:01 -0700

    change

--- index.js
+++ index.js
@@ -12,12 +12,11 @@ mongoose.connect(config.MONGOODB_URL, (err) => {
   console.log('connected to db successfully')
 })
 
-
-// Add headers before the routes are defined
+// Add headers
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
